f1690ae1796bca16c0810054dd1c7ba936b7ae72
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2023-10-14 13:00:03 +0100

    Move the story chapters to a new file.
    I ran
      $ bazel run //:chapter_love_and_death_test
      $ bazel run @rules_rust//:rustfmt
    and it worked as expected.

--- BUILD
+++ BUILD
@@ -1,11 +1,17 @@
 load("@rules_rust//rust:defs.bzl", "rust_binary", "rust_library", "rust_test")
 
 rust_binary(
+    name = "storyteller_main",
+    srcs = ["storyteller.rs"],
+)
+
+rust_library(
     name = "storyteller",
     srcs = ["storyteller.rs"],
 )
 
 rust_test(
-    name = "storyteller_test",
-    srcs = ["storyteller.rs"],
+    name = "chapter_love_and_death_test",
+    srcs = ["chapters/love_and_death.rs"],
+    deps = [":storyteller"],
 )
--- chapters/love_and_death.rs
+++ chapters/love_and_death.rs
@@ -1,34 +1,11 @@
 // This is the storyteller game model.
 use std::collections::VecDeque;
-
-pub enum Name {
-    Adam,
-    Eve,
-    Nobody,
-}
-
-pub enum Scene {
-    Love { left: Name, right: Name },
-    Death { grave: Name, witness: Name },
-}
-
-pub enum Event {
-    InLove { me: Name, partner: Name },
-    Dies { me: Name },
-    IsHeartbroken { me: Name },
-}
-
-pub type Story = VecDeque<Scene>;
-pub type Log = VecDeque<Event>;
-
-fn main() {
-    println!("hello world!");
-}
+use storyteller::Name::{Adam, Eve, Nobody};
+use storyteller::Scene;
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::Name::*;
 
     #[test]
     fn test_eve_dies_heartbroken() {
