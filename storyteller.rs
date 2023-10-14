b5a0cf7df35e48d68a8a276c6237502196df13c5
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2023-10-14 12:36:51 +0100

    Add Love and Death scenes.

--- storyteller.rs
+++ storyteller.rs
@@ -1,4 +1,18 @@
 // This is the storyteller game model.
+use std::collections::VecDeque;
+
+enum Name {
+    Adam,
+    Eve,
+    Nobody
+}
+
+enum Scene {
+    Love { left: Name, right : Name },
+    Death { grave: Name, witness: Name },
+}
+
+type Story = VecDeque<Scene>;
 
 fn main() {
     println!("hello world!");
@@ -6,13 +20,26 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::Name::*;
+
     #[test]
-    fn test_one_plus_two() {
-        assert_eq!(1 + 2, 3);
+    fn test_everyone_dies() {
+        let storydeck: VecDeque<Scene> = VecDeque::from(vec![
+            Scene::Love { left: Adam, right: Eve },
+            Scene::Death { grave: Adam, witness: Eve}, 
+            Scene::Death { grave: Eve, witness: Nobody},
+        ]);
+        assert_eq!(storydeck.len(), 3);
     }
 
     #[test]
-    fn test_one_plus_one() {
-        assert_eq!(1 + 1, 2);
+    fn test_seeing_the_ghost_of_a_lover() {
+        let storydeck: VecDeque<Scene> = VecDeque::from(vec![
+            Scene::Love { left: Adam, right: Eve },
+            Scene::Death { grave: Adam, witness: Eve}, 
+            Scene::Love { left: Adam, right: Eve },
+        ]);
+        assert_eq!(storydeck.len(), 3);
     }
 }
