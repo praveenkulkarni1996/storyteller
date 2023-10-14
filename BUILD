ff76a6c211b0e7a1bc9e502e3292dfbdf106172c
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2023-10-14 12:27:32 +0100

    Initial commit for bazel + rust.

---
+++ BUILD
@@ -0,0 +1,11 @@
+load("@rules_rust//rust:defs.bzl", "rust_binary", "rust_library", "rust_test")
+
+rust_binary(
+    name = "storyteller",
+    srcs = ["storyteller.rs"],
+)
+
+rust_test(
+    name = "storyteller_test",
+    srcs = ["storyteller.rs"],
+)
---
+++ storyteller.rs
@@ -0,0 +1,18 @@
+// This is the storyteller game model.
+
+fn main() {
+    println!("hello world!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_one_plus_two() {
+        assert_eq!(1 + 2, 3);
+    }
+
+    #[test]
+    fn test_one_plus_one() {
+        assert_eq!(1 + 1, 2);
+    }
+}
