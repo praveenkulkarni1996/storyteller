cf901d47e2d84773da9d949dd9e2d5cb59101b94
Praveen Kulkarni <praveenkulkarni1996@gmail.com>
2023-10-14 17:04:14 +0100

    Create "Eve dies Heartbroken" gameplay function.
    See https://www.youtube.com/watch?v=zeaqcwwhKkY&t=228s

--- chapters/love_and_death.rs
+++ chapters/love_and_death.rs
@@ -1,8 +1,33 @@
 // This is the storyteller game model.
-use storyteller::Event::*;
 use storyteller::Log;
 use storyteller::Name::{Adam, Eve, Nobody};
 use storyteller::Scene;
+use storyteller::Story;
+
+#[derive(Eq, PartialEq, Debug, Clone)]
+enum Status {
+    Ok,
+    Err(String),
+}
+
+fn eve_dies_heartbroken(story: Story) -> Status {
+    let events: Log = storyteller::act(&story);
+    // Story must only have 3 scenes or fewer.
+    if story.len() > 3 {
+        return Status::Err(String::from("story has more than 3 scenes."));
+    }
+    // Eve must be dead.
+    if storyteller::is_alive(&events, Eve) {
+        return Status::Err(String::from("Eve is still alive"));
+    }
+    // Eve must be heartbroken.
+    if !storyteller::is_heartbroken(&events, Eve) {
+        return Status::Err(String::from("eve is not heartbroken"));
+    }
+    // TODO: Story must only use the scenes `Love` and `Death`.
+    // TODO: Story must only use the characters `Adam` and `Eve`.
+    return Status::Ok;
+}
 
 #[cfg(test)]
 mod tests {
@@ -24,23 +49,7 @@ mod tests {
                 witness: Nobody,
             },
         ];
-        let events: Log = vec![
-            InLove {
-                me: Adam,
-                with: Eve,
-            },
-            InLove {
-                me: Eve,
-                with: Adam,
-            },
-            Dies { me: Adam },
-            IsHeartbroken { me: Eve },
-            Dies { me: Eve },
-            IsHeartbroken { me: Nobody },
-        ];
-        assert_eq!(storyteller::act(&storydeck), events);
-        assert!(!storyteller::is_alive(&events, Eve)); // Eve dies.
-        assert!(storyteller::is_heartbroken(&events, Eve)); // Eve is heartbroken
+        assert_eq!(eve_dies_heartbroken(storydeck), Status::Ok);
     }
 
     #[test]
